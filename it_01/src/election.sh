9e06ce4799cc76cbadaed08c8ac98db740c2993a
h4rrytrum4n <h4rrytrum4n@zedat.fu-berlin.de>
2017-05-11 19:10:57 +0200

    Add new file

---
+++ it_01/src/election.sh
@@ -0,0 +1,19 @@
+hidden@hidden-VirtualBox:/var/www/html$ sudo -i -u postgres
+postgres@hidden-VirtualBox:~$ ls
+9.6
+postgres@hidden-VirtualBox:~$ psql
+psql (9.6.2)
+Type "help" for help.
+postgres=# create database election;
+CREATE DATABASE
+postgres=# \list
+                                  List of databases
+   Name    |  Owner   | Encoding |   Collate   |    Ctype    |   Access privileges   
+-----------+----------+----------+-------------+-------------+-----------------------
+ election  | postgres | UTF8     | en_US.UTF-8 | en_US.UTF-8 | 
+ postgres  | postgres | UTF8     | en_US.UTF-8 | en_US.UTF-8 | 
+ template0 | postgres | UTF8     | en_US.UTF-8 | en_US.UTF-8 | =c/postgres          +
+           |          |          |             |             | postgres=CTc/postgres
+ template1 | postgres | UTF8     | en_US.UTF-8 | en_US.UTF-8 | =c/postgres          +
+           |          |          |             |             | postgres=CTc/postgres
+(4 rows)
